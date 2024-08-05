5e1137e52dc2ce89711affdd553cac9953603bdc
xijin285 <162889763+xijin285@users.noreply.github.com>
2024-08-05 15:16:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "cc.anto.us.kg": dockerHub,
+  "quay.anto.us.kg": "https://quay.io",
+  "gcr.anto.us.kg": "https://gcr.io",
+  "k8s-gcr.anto.us.kg": "https://k8s.gcr.io",
+  "k8s.anto.us.kg": "https://registry.k8s.io",
+  "ghcr.anto.us.kg": "https://ghcr.io",
+  "cloudsmith.anto.us.kg": "https://docker.cloudsmith.io",
+  "ecr.anto.us.kg": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.anto.us.kg": dockerHub,
 };
 
 function routeByHosts(host) {
